ecbf4864e119b1ae2f763943a38390bfd7f999e6
SergeyXP <s.pusthotin@gmail.com>
2022-08-16 20:54:45 +0400

    005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string userName=Console.ReadLine();
+
+if (userName.ToLower()=="маша")
+{
+    Console.WriteLine("Ура!!! Маша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(userName);    
+}
