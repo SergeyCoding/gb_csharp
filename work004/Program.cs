dd0df0a7415b14225e778104ce50d7ebc4f61fc3
SergeyXP <s.pusthotin@gmail.com>
2022-08-18 22:46:22 +0400

    Work 004

--- work004/Program.cs
+++ work004/Program.cs
@@ -2,10 +2,17 @@
 Console.Write("Число: ");
 int x = int.Parse(Console.ReadLine());
 
+if (x < 0)
+    x = -x;
+
 int s = -x;
 
 while (s <= x)
 {
-    Console.WriteLine(s);
+    Console.Write(s);
+
+    if (s != x)
+        Console.Write(", ");
+
     s++;
 }
