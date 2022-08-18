cb50400a5749ab95f0703a4ddef998a7c6a57029
SergeyXP <s.pusthotin@gmail.com>
2022-08-18 22:05:20 +0400

    Work 002 003

---
+++ work002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите 1 число: ");
+
+// С терминала мы вводим строку, но надо присвоить значение в целую переменную
+// этто делает parse
+
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int x2 = int.Parse(Console.ReadLine());
+
+if (x1 == x2 * x2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+//Все ок?
+
