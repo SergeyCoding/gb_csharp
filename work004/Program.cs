01777c5b2c30615c105721b3ce16489ba00cf944
SergeyXP <s.pusthotin@gmail.com>
2022-08-18 22:38:04 +0400

    Work 004 005

---
+++ work004/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Число: ");
+int x = int.Parse(Console.ReadLine());
+
+int s = -x;
+
+while (s <= x)
+{
+    Console.WriteLine(s);
+    s++;
+}
---
+++ work005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x >= 100 && x <= 999)
+{
+    // Выведем остаток от деления на 10
+    Console.WriteLine(x % 10);
+}
+else
+    Console.WriteLine("Это не трехначное число");
