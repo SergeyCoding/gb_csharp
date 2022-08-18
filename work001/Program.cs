86fd4a9b5afe7fc94b3b61ef732f6838fbc82c94
SergeyXP <s.pusthotin@gmail.com>
2022-08-18 21:33:09 +0400

    work 01

---
+++ work001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+
+var number = int.Parse(Console.ReadLine() ?? string.Empty);
+var sqr = number * number;
+Console.WriteLine($"Результат = {sqr}");
+Console.WriteLine($"Результат = {Math.Pow(number, 2)}");
+
+
