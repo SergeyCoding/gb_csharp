baef1aafca122bfeb427c62fc18b02d0cb75a693
SergeyXP <s.pusthotin@gmail.com>
2022-08-20 13:42:34 +0400

    Lecture 02

---
+++ Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int i = 0;
+    while (i < collection.Length)
+    {
+        collection[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int i = 0;
+    while (i < coll.Length)
+    {
+        System.Console.WriteLine(coll[i]);
+        i++;
+    }
+}
+
+Console.WriteLine("Example 12 ArrayLibrary");
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
