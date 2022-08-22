42229b53ad5c93cb90dcd920f6abaadb67116d94
SergeyXP <s.pusthotin@gmail.com>
2022-08-22 22:22:11 +0400

    Seminar 02 Task 14

---
+++ work214/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.Write("Число: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x % 7 == 0 && x % 23 == 0)
+    System.Console.WriteLine("да");
+else
+    System.Console.WriteLine("нет");
