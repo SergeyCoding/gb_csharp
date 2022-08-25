8c6435d6430c7f032d708b433ee6b78b8fba08a0
SergeyXP <s.pusthotin@gmail.com>
2022-08-25 22:14:15 +0400

    work322

---
+++ work322/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int n = 1;
+
+while (n <= number)
+{
+    Console.Write(n * n);
+
+    if (n < number)
+        Console.Write(", ");
+
+    n++;
+}
+
+Console.WriteLine();
