f918a69c9e32b030640343805d050bc862dad691
SergeyXP <s.pusthotin@gmail.com>
2022-08-25 21:25:09 +0400

    Work318

---
+++ work318/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите четверть: ");
+int quarta = int.Parse(Console.ReadLine());
+
+if (quarta < 1 || quarta > 4)
+{
+    System.Console.WriteLine("Ошибка! Число должно быть в диапазоне 1-4");
+}
+else
+{
+    if (quarta == 1)
+        System.Console.WriteLine("x > 0 && y > 0");
+    else if (quarta == 2)
+        System.Console.WriteLine("x < 0 && y > 0");
+    else if (quarta == 3)
+        System.Console.WriteLine("x < 0 && y < 0");
+    else
+        System.Console.WriteLine("x > 0 && y < 0");
+}
