468a67f84df323838ea6b7d148d1f4c62b6557ab
SergeyXP <s.pusthotin@gmail.com>
2022-08-25 21:09:50 +0400

    Seminar 03

---
+++ work216/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Первое число:");
+int x1 = int.Parse(Console.ReadLine());
+System.Console.Write("Второе число:");
+int x2 = int.Parse(Console.ReadLine());
+
+if (x1 * x1 == x2 || x1 == x2 * x2)
+    System.Console.WriteLine("да");
+else
+    System.Console.WriteLine("нет");
---
+++ work317/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    System.Console.WriteLine(1);
+else if (x < 0 && y > 0)
+    System.Console.WriteLine(2);
+else if (x < 0 && y < 0)
+    System.Console.WriteLine(3);
+else
+    System.Console.WriteLine(4);
