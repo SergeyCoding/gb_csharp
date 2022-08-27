171fd5a7c8a195c0bba6afa37aa7ced972d360a0
SergeyXP <s.pusthotin@gmail.com>
2022-08-27 13:47:12 +0400

    Lecture03 ended

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -32,3 +32,49 @@ string Method4(Int16 count, char c)
 
 Method4(10, '*');
 
+
+// for
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j < 10; j++)
+    {
+        System.Console.Write(i * j + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+PrintArray(arr);
+
+void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPos])
+            {
+                minPos = j;
+            }
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minPos];
+        arr[minPos] = temp;
+    }
+}
+
+SelectionSort(arr);
+PrintArray(arr);
