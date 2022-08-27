9484a931f4d31b5df7966a197245653ca6b84372
SergeyXP <s.pusthotin@gmail.com>
2022-08-27 13:13:38 +0400

    Lecture03

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS8321
+// Вид 1
+void Method1()
+{
+    System.Console.WriteLine("");
+}
+
+Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+
+Method2("Text");
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+System.Console.WriteLine(Method3());
+
+// Вид 3
+string Method4(Int16 count, char c)
+{
+    int i = 0;
+    string result = string.Join('-', c);
+    return result;
+}
+
+Method4(10, '*');
+
