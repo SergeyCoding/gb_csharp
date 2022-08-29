c90fd43a51c5a6fbb9fa62948c83079584e0e62b
SergeyXP <s.pusthotin@gmail.com>
2022-08-29 22:09:35 +0400

    Work430

---
+++ work430/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] RandomArray()
+{
+    int[] arr = new int[8];
+
+    for (int i = 0; i < 8; i++)
+        arr[i] = new Random().Next(0, 2);
+
+    return arr;
+}
+
+void PrintArray(int[] coll)
+{
+    int i = 0;
+    while (i < coll.Length)
+    {
+        System.Console.Write($"{coll[i]} ");
+        i++;
+    }
+}
+
+PrintArray(RandomArray());
