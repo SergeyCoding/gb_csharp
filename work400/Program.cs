ef32c330b2649bee1a49059c90b2e68670d97825
SergeyXP <s.pusthotin@gmail.com>
2022-08-29 21:09:28 +0400

    Seminar04

---
+++ work400/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetNums(int number)
+{
+    int sum = 0;
+
+    for (int i = 0; i < number; i++)
+        sum += i;
+
+    return sum;
+}
+
+System.Console.WriteLine($"{GetNums(num)}");
